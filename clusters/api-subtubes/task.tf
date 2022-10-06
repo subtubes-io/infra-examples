488dd5977e5b0f038f7c3518b544dbefeeec8146
Edgar Martinez <edgarhmartinez03@gmail.com>
2022-10-06 07:41:47 -0700

    Add task

---
+++ clusters/api-subtubes/task.tf
@@ -0,0 +1,48 @@
+resource "aws_ecs_task_definition" "api_subtubes" {
+  container_definitions = jsonencode(
+    [
+      {
+        cpu         = 128
+        environment = []
+        essential   = true
+        image       = "568949616117.dkr.ecr.us-west-2.amazonaws.com/subtubes-api:latest"
+        logConfiguration = {
+          logDriver = "awslogs"
+          options = {
+            awslogs-group         = "/ecs/api-subtubes"
+            awslogs-region        = "us-west-2"
+            awslogs-stream-prefix = "ecs"
+          }
+        }
+        memoryReservation = 128
+        mountPoints       = []
+        name              = "api-subtubes"
+        portMappings = [
+          {
+            containerPort = 6060
+            hostPort      = 6060
+            protocol      = "tcp"
+          },
+        ]
+        volumesFrom = []
+      },
+    ]
+  )
+  cpu                = "256"
+  execution_role_arn = "arn:aws:iam::568949616117:role/ecsTaskExecutionRole"
+  family             = "api-subtubes"
+
+  memory             = "512"
+  network_mode       = "awsvpc"
+  requires_compatibilities = [
+    "FARGATE",
+  ]
+  
+  tags          = {}
+  tags_all      = {}
+  task_role_arn = "arn:aws:iam::568949616117:role/api-subtubes-task-role"
+
+  runtime_platform {
+    operating_system_family = "LINUX"
+  }
+}
